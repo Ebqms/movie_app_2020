4cf90a338d0fba80dbbae0d12d8714689d5e94b1
Leemit <ghahn97@gmail.com>
2021-05-24 15:54:03 +0900

    02-2 깃허브에 리엑트 앱 업로드하기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Food({name, image, rating}){
+  return(
+    <div>
+      <h3> I like {name}</h3>
+      <h4>{rating}/5.0</h4>
+      <img src={image} alt={name} />
+    </div>
+  ); 
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name: 'kimchi',
+    image: 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png' 
+  },
+  {
+    id: 2,
+    name: 'samgyupsal',
+    image: 'https://cdn.mindgil.com/news/photo/202103/70839_7148_1250.jpg'
+  }
+];
+
+// function renderFood(dish){
+//   return <Food name={dish.name} picture={dish.image}/>;
+// }
+
+// const renderFood = dish => <Food name={dish.name} picture={dish.image}/>;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // return <div className="App"/> 
+  return(
+  <div>
+    <h1>hello</h1>
+    {foodILike.map(dish =>
+      (<Food key={dish.id} name={dish.name} image={dish.image} rating={dish.rating}/>
+      ))};
+  </div>
+  ); 
 }
+
+Food.propTypes = {
+  name : PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
 
 export default App;
