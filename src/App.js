400c7425504309131875242e5ba99dfc54a425b9
Leemit <ghahn97@gmail.com>
2021-07-24 13:11:35 +0900

    App style

--- src/App.js
+++ src/App.js
@@ -1,35 +1,76 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './movie';
+import styled from 'styled-components';
 
-class Loading extends React.Component{
-  state ={
-    isLoading : true,
+const Wrapper = styled.section`
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  height: 100%;
+  background: #eff3f7;
+  
+`;
+
+class Loading extends React.Component {
+  state = {
+    isLoading: true,
     movies: [],
   };
 
-  componentDidMount(){
-    //데이터 로딩
-    setTimeout(()=> {
-      this.setState({isLoading : false});
-    }, 6000);
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false })
   }
 
-  render(){
-    const {isLoading} = this.state;
+
+  componentDidMount() {
+    //데이터 로딩
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
 
     return (
-      <>
-        {isLoading ? 'Loading...' : 'We are ready!!!'}
-      </>
+      <Wrapper className="container">
+
+        {isLoading ?
+          <div className="loader">
+            <span className="loader__text"> Loading </span>
+          </div>
+          : (
+            <div className="movies">
+              {movies.map(m => (
+                <Movie key={m.id} id={m.id} year={m.year} title={m.title} summary={m.summary} poster={m.medium_cover_image} genres={m.genres} />
+              ))}
+            </div>
+          )}
+      </Wrapper>
     )
   }
 }
 
-function App(){
+function App() {
   return (
     <>
-      <Loading/>
+      <Loading />
     </>
   );
 }
 
 export default App;
+
+
+// memo
+/*
+yts API : https://yts.mx/api/v2/list_movies.json
+nicolas yts porxy github : https://github.com/serranoarevalo/yts-proxy/
+nicolas yts Proxy API : https://yts-proxy.now.sh/list_movies.json
+
+*/
---
+++ src/movie.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  background-color: white;
+  margin-bottom: 70px;
+  font-weight: 300;
+  padding: 20px;
+  border-radius: 5px;
+  color: #adaeb9;
+  box-shadow: 0 13px 27px -5px rgba(50, 50, 93, 0.25),
+    0 8px 16px -8px rgba(0, 0, 0, 0.3), 0 -6 16px -6 rgba(0, 0, 0, 0.025);
+
+  a {
+    display: grid;
+    grid-template-columns: minmax(150px, 1fr) 2fr;
+    grid-gap: 20px;
+    text-decoration: none;
+    color: inherit;
+  }
+
+  img {
+    position: relative;
+    top: -50px;
+    max-width: 150px;
+    width: 100%;
+    margin-right: 30px;
+    box-shadow: 0 30px 60px -12px rgba(50, 50, 93, 0.25),
+      0 18px 36px -18px rgba(0, 0, 0, 0.3),
+      0 -12px 36px -8px rgba(0, 0, 0, 0.025);
+  }
+
+  .movie__data h3,
+  .movie__data h5 {
+      margin: 0;
+      font-weight: 300;
+  }
+
+  .movie__data h3{
+      margin-bottom: 5px;
+      font-size: 24px;
+      color: #2c2c2c;
+  }
+
+  .movie__data .movie__genres{
+      list-style: none;
+      padding: 0;
+      margin: 0;
+      display: flex;
+      flex-wrap: wrap;
+      margin: 5px 0;
+  }
+
+  .movie__genres li,
+  .movie__data h5{
+      margin-right: 10px;
+      font-size: 14px;
+  }
+
+
+
+`;
+
+function Movie({ title, year, summary, poster, genres }) {
+  return (
+    <Wrapper>
+      <img src={poster} alt="" />
+      <div className="movie__data">
+        <h3>{title}</h3>
+        <h5>{year}</h5>
+        <p>{summary.slice(0, 180)}...</p>
+        <ul className="movie__genres">
+          {genres.map((g, i) => {
+            return (
+              <li key={i} className="movie__genre">
+                {g}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </Wrapper>
+  );
+}
+
+Movie.propTypes = {
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
